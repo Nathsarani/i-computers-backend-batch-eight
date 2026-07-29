2ba908f348aaa9f9407bca84765bf823c9d14139
Nathsarani <prabhashi@gmail.com>
2026-07-29 16:32:43 +0530

    fix error

--- controllers/userController.js
+++ controllers/userController.js
@@ -34,6 +34,17 @@ const transporter = nodemailer.createTransport({
 		user: "wensoftone@gmail.com",
 		pass: process.env.GMAIL_APP_PASSWORD,
 	},
+	connectionTimeout: 10000,
+	socketTimeout: 10000,
+});
+
+transporter.verify((error, success) => {
+	if(error){
+		console.log("SMTP ERROR:", error);
+	}
+	else{
+		console.log("SMTP READY");
+	}
 });
 
 export function createUser(req,res){
